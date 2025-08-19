b5d8b07973d36400a029fab0ce4aa9ac79218399
Mitchel <mitchel_jager@hotmail.com>
2025-08-19 02:05:53 +0200

    feat: Add all genres to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def index
+    @genres = Genre.where.not(books_count: 0).order(name: :asc)
     @hot_books = Book.overview_join.order(hotness: :desc).limit(8)
     @mystery_books = Genre.find_by_slug("mysterie").books.overview_join.order(hotness: :desc).limit(8)
     @fantasy_books = Genre.find_by_slug("fantasy").books.overview_join.order(hotness: :desc).limit(8)
