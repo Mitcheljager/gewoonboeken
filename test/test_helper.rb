e74bd84d8db8fe5fef471f4a7169bf928e2244c9
Mitchel <mitchel_jager@hotmail.com>
2025-08-23 19:43:27 +0200

    test: Stub assets
    Vite assets don't seem to load correctly in tests. I can't say I know why that is, but this fixes that by stubbing them to various other tags.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,3 +13,56 @@ module ActiveSupport
     # Add more helper methods to be used by all tests here...
   end
 end
+
+module InlineSvg
+  class ViteFileLoader
+    def self.named(filename)
+      '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"><rect width="16" height="16" fill="#ccc"/></svg>'
+    end
+  end
+end
+
+module ViteRails::TagHelpers
+  def vite_asset_path(name, **options)
+    filename = File.basename(name)
+    "/vite-test/assets/#{filename}"
+  end
+
+  def vite_image_path(name, **options)
+    filename = File.basename(name)
+    "/vite-test/assets/#{filename}"
+  end
+
+  def vite_javascript_path(name, **options)
+    filename = File.basename(name)
+    "/vite-test/assets/#{filename}"
+  end
+
+  def vite_stylesheet_path(name, **options)
+    filename = File.basename(name)
+    "/vite-test/assets/#{filename}"
+  end
+
+  def vite_javascript_tag(name)
+    filename = File.basename(name)
+    javascript_include_tag("/vite-test/assets/#{filename}")
+  end
+
+  def vite_stylesheet_tag(name, **options)
+    filename = File.basename(name)
+    stylesheet_link_tag("/vite-test/assets/#{filename}", options)
+  end
+
+  def vite_image_tag(name, **options)
+    filename = File.basename(name)
+    image_tag("/vite-test/assets/#{filename}", options)
+  end
+
+  def vite_client_tag
+    ""
+  end
+
+  def vite_typescript_tag(name)
+    vite_javascript_tag(name)
+  end
+end
