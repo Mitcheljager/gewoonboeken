3bfbdeef9427c7f1f76e03160c45256d7fc256af
Mitchel <mitchel_jager@hotmail.com>
2025-08-24 20:08:43 +0200

    chore: Formatting

--- test/controllers/api/base_controller_test.rb
+++ test/controllers/api/base_controller_test.rb
@@ -6,20 +6,20 @@ class Api::BaseControllerTest < ActionDispatch::IntegrationTest
     ENV["INTERNAL_API_TOKEN"] = @token
   end
 
-  test "allows request with correct token" do
+  test "Allows request with correct token" do
     get api_ping_url, headers: { "Token" => @token }
 
     assert_response :success
     assert_equal({ "message" => "pong" }, JSON.parse(response.body))
   end
 
-  test "rejects request with missing token" do
+  test "Rejects request with missing token" do
     get api_ping_url
 
     assert_response :unauthorized
   end
 
-  test "rejects request with invalid token" do
+  test "Rejects request with invalid token" do
     get api_ping_url, headers: { "Token" => "wrong-token" }
 
     assert_response :unauthorized
