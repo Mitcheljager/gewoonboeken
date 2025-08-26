ac068b3e898e05736d6c30cc3f138502ab6105f2
Mitchel <mitchel_jager@hotmail.com>
2025-08-26 12:38:28 +0200

    fix(amazon-retourdeals): Use correct syntax when checking for shipping

--- scraper/sources/amazon_retourdeals.rb
+++ scraper/sources/amazon_retourdeals.rb
@@ -1,4 +1,5 @@
 require_relative "../get_document"
+require_relative "../helpers/log_message"
 
 def scrape_amazon_retourdeals(isbn)
   listing = find_listing_for_isbn_and_source_name(isbn, "Amazon") # Deliberately get Amazon rather than Amazon Retourdeals. If Amazon contain no listing, than retourdeals does not either
@@ -18,7 +19,7 @@ def scrape_amazon_retourdeals(isbn)
   return { url: nil, available: false } unless url.include?("/dp/") || document.nil?
 
   used_item_node = document.at_css("#usedAccordionRow")
-  has_amazon_retour_deals = used_item_node&.text.include?("Amazon RetourDeals")
+  has_amazon_retour_deals = used_item_node&.text&.include?("Amazon RetourDeals")
 
   if !has_amazon_retour_deals
     puts "Amazon page for \"#{isbn}\" does not contain RetourDeals offer"
@@ -26,7 +27,7 @@ def scrape_amazon_retourdeals(isbn)
   end
 
   price = used_item_node.at_css(".a-offscreen")&.text&.tr(",", ".")&.tr("€", "")&.strip
-  price_includes_shipping = used_item_node.text.includes?("GRATIS bezorging")
+  price_includes_shipping = used_item_node.text.include?("GRATIS bezorging")
 
   { url:, price:, price_includes_shipping:, condition: :used, available: true }
 end
