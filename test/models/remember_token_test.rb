4dd31b675ae63961f9cbfb52bc8f52b64fac1afa
Mitchel <mitchel_jager@hotmail.com>
2025-09-03 20:18:20 +0200

    test: Add tests for RememberToken validations

--- test/models/remember_token_test.rb
+++ test/models/remember_token_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class RememberTokenTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @remember_token = remember_tokens(:one)
+  end
+
+  test "Requires token" do
+    @remember_token.token = nil
+
+    assert_not @remember_token.valid?
+    assert_includes @remember_token.errors[:token], "can't be blank"
+  end
+
+  test "Belongs to a user" do
+    assert_kind_of User, @remember_token.user
+  end
 end
