4d12207962b0abe16e486dcd138ff3e3dc05607d
Mitchel <mitchel_jager@hotmail.com>
2025-09-03 20:16:04 +0200

    test: Add tests for indexListingUrl validations

--- test/models/indexed_listing_url_test.rb
+++ test/models/indexed_listing_url_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class IndexedListingUrlTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @url = indexed_listing_urls(:one)
+  end
+
+  test "Requires source_name" do
+    @url.source_name = nil
+
+    assert_not @url.valid?
+    assert_includes @url.errors[:source_name], "can't be blank"
+  end
+
+  test "Requires isbn" do
+    @url.isbn = nil
+
+    assert_not @url.valid?
+    assert_includes @url.errors[:isbn], "can't be blank"
+  end
+
+  test "isbn must be 13 digits" do
+    @url.isbn = "12345"
+
+    assert_not @url.valid?
+    assert_includes @url.errors[:isbn], "is invalid"
+
+    @url.isbn = "9781234567890"
+    assert @url.valid?
+  end
+
+  test "Requires url" do
+    @url.url = nil
+
+    assert_not @url.valid?
+    assert_includes @url.errors[:url], "can't be blank"
+  end
 end
