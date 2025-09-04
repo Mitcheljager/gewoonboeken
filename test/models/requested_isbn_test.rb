df82be20e5dfcb3c5f12240acae47ba7441a1652
Mitchel <mitchel_jager@hotmail.com>
2025-09-04 23:31:19 +0200

    test: Add tests for RequestedIsbn

--- test/models/requested_isbn_test.rb
+++ test/models/requested_isbn_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
-class RequestedISBNTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class RequestedIsbnTest < ActiveSupport::TestCase
+  def setup
+    @item = RequestedISBN.new(isbn: "1234567890123", status: :not_found)
+  end
+
+  test "Requires valid status" do
+    assert_raises ArgumentError do
+      @item.status = "fake_status"
+    end
+  end
+
+  test "Enum generates helper methods" do
+    assert_respond_to @item, :not_found!
+    assert_respond_to @item, :error!
+    assert_respond_to @item, :rejected!
+    assert_respond_to @item, :resolved!
+  end
+
+  test "Can change status using enum" do
+    @item.error!
+
+    assert_equal "error", @item.status
+  end
 end
