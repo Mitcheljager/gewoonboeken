f4043723af462504442d27f3c52e3d542f0ddcc3
Mitchel <mitchel_jager@hotmail.com>
2026-02-28 12:56:33 +0100

    fix: Get subgenres only once

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,9 +2,11 @@ class GenresController < ApplicationController
   before_action :set_genre, only: [:show]
 
   def show
-    @hot_books = @genre.books.overview_join.includes(:authors, :listings).order(hotness: :desc).limit(8)
-    @subgenres = @genre.subgenres.order(books_count: :desc).limit(4)
-    @genre_links = @genre.subgenres.select(:name, :slug, :books_count).where.not(books_count: 0)
+    @hot_books = @genre.books.overview_join.order(hotness: :desc).limit(8)
+
+    subgenres = @genre.subgenres.where.not(books_count: 0)
+    @subgenres = subgenres.order(books_count: :desc).limit(4)
+    @genre_links = subgenres.select(:name, :slug, :books_count).where.not(books_count: 0)
   end
 
   private
