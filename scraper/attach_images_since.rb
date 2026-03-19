e8a32688d5c846ea1283d72325b320795e3d684a
Mitchel <mitchel_jager@hotmail.com>
2026-03-20 00:39:55 +0100

    feat(scraper): Add script to re-attach images since time period
    I ran the scraper and a whole bunch of images failed to create because imagemagick was not installed

---
+++ scraper/attach_images_since.rb
@@ -0,0 +1,26 @@
+require_relative "../config/environment"
+require_relative "data/goodreads"
+require_relative "get_book"
+require_relative "attach_remote_image"
+
+hours_ago = ARGV[0].to_i
+
+hours_ago_time = Time.now - hours_ago.hours
+books = Book.where('last_scrape_finished_at < ? OR last_scrape_finished_at IS NULL', hours_ago_time)
+
+books.each_with_index do |book, index|
+  puts "(#{index + 1} / #{books.size}) Finding and attaching image for #{book.isbn}..."
+
+  book = get_book(book.isbn)
+  data = get_goodreads_data(book.isbn)
+
+  if data[:image_url].present?
+    begin
+      attach_remote_image(book, data[:image_url])
+    rescue => error
+      puts error
+    end
+  else
+    puts "No valid image url was found for \"#{book.title}\" (#{book.isbn})"
+  end
+end
