066d0f9a9ae42d96aaf2da18e0a3388367d67fff
Mitchel <mitchel_jager@hotmail.com>
2026-07-04 23:52:51 +0200

    chore(scraper): Reduce frequency for non-hot books in continous scraper

--- scraper/continuously_run_all_scrapers.rb
+++ scraper/continuously_run_all_scrapers.rb
@@ -1,6 +1,6 @@
 loop do
   script_path = File.expand_path("run_all_scrapers.rb", __dir__)
-  system("ruby", script_path, "hours_ago=72")
+  system("ruby", script_path, "hours_ago=96")
 
   sleep(3600)
 end
